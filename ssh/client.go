57ea09b3322a1a57449577b867aa760941329742
rmarken5 <marken.ryan@gmail.com>
2022-11-13 13:08:55 -0500

    Refactor

--- ssh/client.go
+++ ssh/client.go
@@ -1,1 +1,33 @@
 package ssh
+
+import cssh "golang.org/x/crypto/ssh"
+
+func NewSSHClient(opts ...Option) (*cssh.Client, error) {
+	options := &Options{
+		username:        "",
+		password:        "",
+		address:         "",
+		networkProtocol: "",
+		hostKeyCallback: cssh.InsecureIgnoreHostKey(),
+		bannerCallback:  cssh.BannerDisplayStderr(),
+	}
+
+	for _, o := range opts {
+		o(options)
+	}
+	config := &cssh.ClientConfig{
+		Config: cssh.Config{},
+		User:   options.username,
+		Auth: []cssh.AuthMethod{
+			cssh.Password(options.password),
+		},
+		HostKeyCallback:   options.hostKeyCallback,
+		BannerCallback:    options.bannerCallback,
+		ClientVersion:     "",
+		HostKeyAlgorithms: nil,
+		Timeout:           0,
+	}
+
+	return cssh.Dial(options.networkProtocol, options.address, config)
+
+}
--- ssh/options.go
+++ ssh/options.go
@@ -14,36 +14,6 @@ type Options struct {
 }
 type Option func(*Options)
 
-func NewSSHClient(opts ...Option) (*cssh.Client, error) {
-	options := &Options{
-		username:        "",
-		password:        "",
-		address:         "",
-		networkProtocol: "",
-		hostKeyCallback: cssh.InsecureIgnoreHostKey(),
-		bannerCallback:  cssh.BannerDisplayStderr(),
-	}
-
-	for _, o := range opts {
-		o(options)
-	}
-	config := &cssh.ClientConfig{
-		Config: cssh.Config{},
-		User:   options.username,
-		Auth: []cssh.AuthMethod{
-			cssh.Password(options.password),
-		},
-		HostKeyCallback:   options.hostKeyCallback,
-		BannerCallback:    options.bannerCallback,
-		ClientVersion:     "",
-		HostKeyAlgorithms: nil,
-		Timeout:           0,
-	}
-
-	return cssh.Dial(options.networkProtocol, options.address, config)
-
-}
-
 func WithUsername(username string) func(options *Options) {
 	return func(options *Options) {
 		options.username = username
