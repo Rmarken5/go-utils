47596ef72e7daa2afa5ecca0761282e11d8e14d0
rmarken5 <marken.ryan@gmail.com>
2022-11-13 12:37:26 -0500

    setting up ssh client

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/rmarken5/go-utils
+
+go 1.18
+
+require golang.org/x/crypto v0.2.0
+
+require golang.org/x/sys v0.2.0 // indirect
---
+++ ssh/options.go
@@ -0,0 +1,81 @@
+package ssh
+
+import (
+	cssh "golang.org/x/crypto/ssh"
+)
+
+type Options struct {
+	username        string
+	password        string
+	address         string
+	networkProtocol string
+	hostKeyCallback cssh.HostKeyCallback
+	bannerCallback  cssh.BannerCallback
+}
+type Option func(*Options)
+
+func NewSSHClientConfig(opts ...Option) (*cssh.Client, error) {
+	options := &Options{
+		username:        "",
+		password:        "",
+		address:         "",
+		networkProtocol: "",
+		hostKeyCallback: cssh.InsecureIgnoreHostKey(),
+		bannerCallback:  cssh.BannerDisplayStderr(),
+	}
+
+	for _, o := range opts {
+		o(options)
+	}
+	config := &cssh.ClientConfig{
+		Config: cssh.Config{},
+		User:   options.username,
+		Auth: []cssh.AuthMethod{
+			cssh.Password(options.password),
+		},
+		HostKeyCallback:   options.hostKeyCallback,
+		BannerCallback:    options.bannerCallback,
+		ClientVersion:     "",
+		HostKeyAlgorithms: nil,
+		Timeout:           0,
+	}
+
+	return cssh.Dial(options.networkProtocol, options.address, config)
+
+}
+
+func WithUsername(username string) func(options *Options) {
+	return func(options *Options) {
+		options.username = username
+	}
+}
+
+func WithPassword(password string) func(*Options) {
+	return func(options *Options) {
+		options.password = password
+	}
+}
+
+func WithAddress(address string) func(*Options) {
+	return func(options *Options) {
+		options.address = address
+	}
+}
+
+func WithNetworkProtocol(protocol string) func(*Options) {
+	return func(options *Options) {
+		options.networkProtocol = protocol
+	}
+}
+
+func WithHostKeyCallback(callback cssh.HostKeyCallback) func(*Options) {
+	return func(options *Options) {
+		options.hostKeyCallback = callback
+	}
+}
+
+func WithBannerCallback(bannerCallback cssh.BannerCallback) func(*Options) {
+	return func(options *Options) {
+		options.bannerCallback = bannerCallback
+	}
+}
